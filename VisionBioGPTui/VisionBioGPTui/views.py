012f878c5435d959815c92790e90cab40ed184f4
crivoi-andrei <crivoi@healper.dk>
2023-11-14 17:24:22 +0100

    Started UI

---
+++ VisionBioGPTui/VisionBioGPTui/views.py
@@ -0,0 +1,29 @@
+import json
+
+from django.http import JsonResponse
+from django.views import View
+from django.views.generic import TemplateView
+from transformers import BioGptTokenizer, BioGptModel, BioGptForCausalLM, pipeline
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+
+class ModelAPIView(View):
+    model_checkpoint = "microsoft/biogpt"
+    tokenizer: BioGptTokenizer = BioGptTokenizer.from_pretrained(model_checkpoint, use_fast=True)
+    model: BioGptModel = BioGptForCausalLM.from_pretrained(model_checkpoint)
+    generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            prompt = data.get("prompt", "")
+            output = self.generator(prompt, max_length=20, num_return_sequences=1, do_sample=True)[0]
+            return JsonResponse({'text': output.get('generated_text', 'An error occurred!')})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    def get(self, request, *args, **kwargs):
+        return JsonResponse({"error": "Only POST requests are allowed"}, status=405)
---
+++ VisionBioGPTui/static/js/index.js
@@ -0,0 +1,60 @@
+(function init() {
+    const btnText = {
+        loading: 'Loading',
+        ready: 'Send'
+    };
+    const submitBtn = document.getElementById('submit');
+    const responseSection = document.getElementById('response');
+    const form = document.getElementById('form');
+
+    async function postPrompt(data) {
+        const {csrfmiddlewaretoken, ...rest} = data;
+        console.log(rest);
+        return await fetch('/api/model/', {
+            method: 'POST',
+            body: JSON.stringify(rest),
+            headers: {
+                'X-CSRFToken': csrfmiddlewaretoken,
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    async function handleResponse(response) {
+        console.log(response.status);
+        if (response.ok) {
+            const {text} = await response.json();
+            responseSection.innerText = text;
+        } else {
+            alert(`Something went wrong!\nResponse code: ${response.status}`)
+            const errorMessage = await response.text();
+            console.error(`Error: ${response.status} - ${errorMessage}`);
+        }
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+
+        setBtnLoadingState();
+
+        const response = await postPrompt(data);
+
+        await handleResponse(response);
+
+        setBtnReadyState();
+    }
+
+    function setBtnLoadingState() {
+        submitBtn.setAttribute('disabled', 'disabled');
+        submitBtn.innerText = btnText.loading;
+    }
+
+    function setBtnReadyState() {
+        submitBtn.removeAttribute('disabled');
+        submitBtn.innerText = btnText.ready;
+    }
+
+    form.addEventListener('submit', handleSubmit);
+})();
